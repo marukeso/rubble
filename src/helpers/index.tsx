f4e1272168b299eabf5c7f7cb30ef70ecc4dbe6a
Kentaro Maruyama <marukeso@gmail.com>
2022-05-13 18:22:20 +0900

    feat: authentication setup

---
+++ src/helpers/index.tsx
@@ -0,0 +1,2 @@
+// export * from './queries'
+export const BACKEND_URL = 'http://localhost:1337'
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,55 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
+import { AppShell, Header, MantineProvider } from '@mantine/core'
+import { NotificationsProvider } from '@mantine/notifications'
+import { NhostClient, NhostNextProvider } from '@nhost/nextjs'
+
+import NavBar from '../components/NavBar'
+import { BACKEND_URL } from '../helpers'
+
+import '../styles/globals.css'
+
+const nhost = new NhostClient({ backendUrl: BACKEND_URL })
+const title = 'Nhost with NextJs'
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
+        <Head>
+          <title>{title}</title>
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        </Head>
+
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: 'light'
+          }}
+        >
+          <NotificationsProvider>
+            <AppShell
+              padding="md"
+              navbar={<NavBar />}
+              header={
+                <Header height={60} p="xs">
+                  {title}
+                </Header>
+              }
+              styles={(theme) => ({
+                main: {
+                  backgroundColor:
+                    theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0]
+                }
+              })}
+            >
+              <Component {...pageProps} />
+            </AppShell>
+          </NotificationsProvider>
+        </MantineProvider>
+    </NhostNextProvider>
+  )
 }
 
 export default MyApp
