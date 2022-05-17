3447babf53588e6a647fb0aea9a82a7847fc2222
Kentaro Maruyama <marukeso@gmail.com>
2022-05-17 17:04:07 +0900

    feat: add storybook

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /\.stor(ies|y).[tj]sx$/,
+      })
+    )
+    // Important: return the modified config
+    return config
+  },
 }
 
 module.exports = nextConfig
