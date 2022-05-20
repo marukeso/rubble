9f6daf012afac2c34b82aae88f01385e1ec92867
Kentaro Maruyama <marukeso@gmail.com>
2022-05-20 18:32:56 +0900

    feat: creates app layout and header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import { AppShell, Header, MantineProvider } from '@mantine/core'
+import { MantineProvider } from '@mantine/core'
 import { NotificationsProvider } from '@mantine/notifications'
 import { NhostClient, NhostNextProvider } from '@nhost/nextjs'
 import Head from 'next/head'
 
-import NavBar from '../components/NavBar'
+import AppLayout from '../layouts/AppLayout'
 
 import type { AppProps } from 'next/app'
 
@@ -12,12 +12,12 @@ import '../styles/globals.css'
 const nhost = new NhostClient({
   backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL as string,
 })
-const title = 'Nhost with NextJs'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
       <Head>
-        <title>{title}</title>
+        <title>rubble</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
@@ -33,25 +33,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         }}
       >
         <NotificationsProvider>
-          <AppShell
-            padding="md"
-            navbar={<NavBar />}
-            header={
-              <Header height={60} p="xs">
-                {title}
-              </Header>
-            }
-            styles={(theme) => ({
-              main: {
-                backgroundColor:
-                  theme.colorScheme === 'dark'
-                    ? theme.colors.dark[8]
-                    : theme.colors.gray[0],
-              },
-            })}
-          >
+          <AppLayout>
             <Component {...pageProps} />
-          </AppShell>
+          </AppLayout>
         </NotificationsProvider>
       </MantineProvider>
     </NhostNextProvider>
